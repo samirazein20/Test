1bf5c92eef3146862aaede64fa2d3659ce577709
Mostafa Mosad <mustafamosad57@gmail.com>
2020-06-28 17:30:53 -0500

    first Commit

---
+++ discoverServer/src/main/java/com/discoverServier/discoverServer/DiscoverServerApplication.java
@@ -0,0 +1,13 @@
+package com.discoverServier.discoverServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServerApplication.class, args);
+	}
+
+}
---
+++ discoverServer/src/test/java/com/discoverServier/discoverServer/DiscoverServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discoverServier.discoverServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
